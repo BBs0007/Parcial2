92ee01006f176a87c0948467c2ebd5e00fff26f6
BBs0007 <bbsbruno.bbs@gmail.com>
2021-10-13 21:05:29 -0400

    creado suerte

---
+++ Parcial2/Models/Suerte.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2.Models
+{
+    public class Suerte
+    {
+        [Key]
+        public string FuturoId { get; set; }
+       // [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+       // public string Vision { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Vision { get; set; }
+        [Url]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Imagen { get; set; }
+    }
+}
