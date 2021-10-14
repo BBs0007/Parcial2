e49ddec7eabab814b77d8bfd4bf2ef8c82843fea
BBs0007 <bbsbruno.bbs@gmail.com>
2021-10-13 21:16:42 -0400

    creado dbcontext

---
+++ Parcial2/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2.Data
+{
+    
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            {
+
+            }
+
+            public DbSet<Suerte> Suerte { get; set; }
+        }
+    
+}
